f85b97bb68a1c9f6ccf9db04d7b7d2c21d83c3f9
Szymon <szymonpaluszewski@gmail.com>
2022-03-03 20:39:14 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace NetZad1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menu1 = new System.Windows.Forms.ComboBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Otwórz Plik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "-";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Zapisz Plik";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "-";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(266, 222);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(293, 115);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(266, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 115);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(269, 387);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(293, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(230, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Klucz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 371);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(385, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "UWAGA - W PRZYPADKU CEZARA NIE MOŻNA UŻYĆ POLSKICH ZNAKÓW";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(266, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Tekst Wczytanego Pliku";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(263, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Tekst Po Zaszyfrowaniu";
+            // 
+            // menu1
+            // 
+            this.menu1.FormattingEnabled = true;
+            this.menu1.Items.AddRange(new object[] {
+            "3DES",
+            "Cezar"});
+            this.menu1.Location = new System.Drawing.Point(43, 136);
+            this.menu1.Name = "menu1";
+            this.menu1.Size = new System.Drawing.Size(148, 21);
+            this.menu1.TabIndex = 18;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(43, 167);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(146, 35);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Szyfruj";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(43, 215);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(148, 33);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "Odszyfruj";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(42, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(144, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Wybierz Metodę Szyfrowania";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.menu1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox menu1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace NetZad1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Wczytywanie plików
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Określanie ścieżki pliku
+                label1.Text = openFileDialog1.FileName;
+
+                //Wczytywanie zawartości pliku do okna tekstowego
+                var fileStream = openFileDialog1.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    textBox1.Text = reader.ReadToEnd();
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Konfiguracja zapisu
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            //Zapisywanie pliku oraz wyświetlenie ścieżki na etykiecie
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, textBox2.Text);
+                label2.Text = saveFileDialog1.FileName;
+            }
+        }
+
+        public string Encrypt(string source, string key)
+        {
+            //Szyfrowanie metodą 3DES https://foxlearn.com/windows-forms/encryption-and-decryption-using-triple-des-in-csharp-376.html
+
+            using (TripleDESCryptoServiceProvider tripleDESCryptoService = new TripleDESCryptoServiceProvider())
+            {
+                using (MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider())
+                {
+                    byte[] byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+                    tripleDESCryptoService.Key = byteHash;
+                    tripleDESCryptoService.Mode = CipherMode.ECB;//CBC, CFB
+                    byte[] data = Encoding.Unicode.GetBytes(source);
+                    return Convert.ToBase64String(tripleDESCryptoService.CreateEncryptor().TransformFinalBlock(data, 0, data.Length));
+                }
+            }
+        }
+
+        public static string Decrypt(string encrypt, string key)
+        {
+            //Szyfrowanie metodą 3DES https://foxlearn.com/windows-forms/encryption-and-decryption-using-triple-des-in-csharp-376.html
+
+
+            using (TripleDESCryptoServiceProvider tripleDESCryptoService = new TripleDESCryptoServiceProvider())
+            {
+                using (MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider())
+                {
+                    byte[] byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+                    tripleDESCryptoService.Key = byteHash;
+                    tripleDESCryptoService.Mode = CipherMode.ECB;//CBC, CFB
+                    byte[] byteBuff = Convert.FromBase64String(encrypt);
+                    return Encoding.Unicode.GetString(tripleDESCryptoService.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+                }
+            }
+        }
+
+        public static char cipher(char ch, int key)
+        {
+
+            // Szyfrowanie Kodem Cezara https://www.c-sharpcorner.com/article/caesar-cipher-in-c-sharp/
+
+
+            if (!char.IsLetter(ch))
+            {
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
+
+
+        }
+
+
+        public static string Encipher(string input, int key)
+        {
+
+            // Szyfrowanie Kodem Cezara https://www.c-sharpcorner.com/article/caesar-cipher-in-c-sharp/
+
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += cipher(ch, key);
+
+            return output;
+        }
+
+        public static string Decipher(string input, int key)
+        {
+
+            // Szyfrowanie Kodem Cezara https://www.c-sharpcorner.com/article/caesar-cipher-in-c-sharp/
+
+            return Encipher(input, 26 - key);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //Boolean sprawdzający czy uzytkownik podał klucz
+            bool bValidKey = ValidateKey();
+            //Boolean sprawdzający czy uzytkownik podał klucz niezawierający liter 
+            bool bValidLet = ValidateLetters();
+
+            if (bValidKey == true)
+            {
+                if (menu1.Text == "3DES")
+                {
+                    //Jeżeli uzytkownik wybrał z listy 3DES szyfrujemy tym algorytmem używając klucza podanego przez użytkowanika
+                    textBox2.Text = textBox1.Text;
+                    textBox2.Text = Encrypt(textBox2.Text, textBox3.Text);
+                }
+
+                else if (menu1.Text == "Cezar")
+                {
+                    //Sprawdzamy czy w kluczu nie ma liter bo gdyby były "Cezar" nie zadziała
+                    if (bValidLet == true)
+                    {
+                        //Jeżeli uzytkownik wybrał z listy "Cezara" szyfrujemy tym algorytmem używając klucza podanego przez użytkowanika
+                        textBox2.Text = textBox1.Text;
+                        textBox2.Text = Encipher(textBox2.Text, Convert.ToInt32(textBox3.Text));
+                    }
+                    //Jeśli w kluczu były litery użytkownik zostaje o tym powiadomiony wysakaującym okienkiem
+                    else MessageBox.Show("Klucz musi być wartością liczbową w przypadku Cezara");
+
+                }
+            }
+            //Jeżeli użytkownik nie podał klucza zostaje o tym powiadomiony wysakaującym okienkiem
+            else MessageBox.Show("Musisz podać klucz");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Kod tutaj jest prawie że identyczny tylko zamiast metod szyfrowania używamy odszyfrowywania
+            bool bValidKey = ValidateKey();
+            bool bValidLet = ValidateLetters();
+
+            if (bValidKey == true)
+            {
+                if (menu1.Text == "3DES")
+                {
+                    textBox2.Text = textBox1.Text;
+                    textBox2.Text = Decrypt(textBox2.Text, textBox3.Text);
+                }
+
+                else if (menu1.Text == "Cezar")
+                {
+                    if (bValidLet == true)
+                    {
+                        textBox2.Text = textBox1.Text;
+                        textBox2.Text = Decipher(textBox2.Text, Convert.ToInt32(textBox3.Text));
+                    }
+                    else MessageBox.Show("Klucz musi być wartością liczbową w przypadku Cezara");
+                }
+            }
+            else MessageBox.Show("Musisz podać klucz");
+        }
+
+        private void textBox3_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ValidateKey();
+        }
+
+        private bool ValidateKey()
+        {
+            //Jeżeli w oknie tekstowym nie ma tekstu użytkownik zobaczy stosowną wiadomość
+            bool keystat = true;
+            if (textBox3.Text == "")
+            {
+                errorProvider1.SetError(textBox3, "Musisz podać klucz");
+                keystat = false;
+            }
+            else
+                errorProvider1.SetError(textBox3, "");
+            return keystat;
+        }
+
+        private bool ValidateLetters()
+        {
+            bool letstat = true;
+            string t = textBox3.Text;
+            //Metoda TryParse sprawdza czy można przkonwertować wartość pola tekstowego na integer
+            if (int.TryParse(t, out _))
+            {
+                //warunek prawdziwy jeśli nie ma liter w tekscie
+                return letstat;
+            }
+            else
+            {
+                letstat = false;
+            }
+            return letstat;
+        }
+
+    }
+
+}
